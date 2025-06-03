53aa3b62c4c01cb8930ceabebc76f3334cee56b0
sumanth338 <sumanthashwin000@gmail.com>
2025-06-03 15:46:41 +0530

    this is my first commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express()
+app.use('/',(req, res)=>{
+	res.send('hello from the server')
+})
+
+app.listen(3000,()=>{
+	console.log('server is running on port 3000')
+})
+
+
